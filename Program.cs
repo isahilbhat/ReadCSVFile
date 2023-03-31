02f6780c012382eb1b51d7c5037a9126d976ee49
isahilbhat <sahil.bhat4748@gmail.com>
2023-03-31 12:14:29 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFilePath = @"D:\\Projects\\ReadFileProject\\readfile2\\sample.csv"; //input file path (change accordingly)
+        string outputFilePath = @"D:\\Projects\\ReadFileProject\\readfile2\\output.csv"; //input file path 
+
+        // Read the CSV file
+        string[] lines = File.ReadAllLines(inputFilePath);
+
+        // Write the output CSV file
+        using (StreamWriter writer = new StreamWriter(outputFilePath))
+        {
+            foreach (string line in lines)
+            {
+                // Split the line into fields
+                string[] fields = line.Split(',');
+
+                //write filtering Logic here
+                if (fields[2] == "28-Apr-22")
+                {
+                    writer.WriteLine(string.Join(',', fields));
+                }
+
+            }
+        }
+
+        Console.WriteLine("Done.");
+    }
+}
